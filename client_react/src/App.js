a7957bf41061a053f06bee96abfa313fc4b95689
André Araujo <andre.arajo07@gmail.com>
2020-07-21 13:59:10 -0300

    Ajuste title

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -23,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React 21
         </a>
       </header>
     </div>
